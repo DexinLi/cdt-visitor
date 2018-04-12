def61c0f70ab0a8f3976cc3b7b3acd7db0cad3bd
DexinLi <ldx94@hotmail.com>
2018-04-12 10:24:04 +0800

    update

--- src/main/java/ParserExample.java
+++ src/main/java/ParserExample.java
@@ -1,195 +1,189 @@
-/*     */ import java.io.PrintStream;
-/*     */ import java.util.HashMap;
-/*     */ import java.util.Map;
-/*     */ import org.eclipse.cdt.core.dom.ast.*;
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */
-/*     */ import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTFunctionDeclarator;
-/*     */ import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTVisibilityLabel;
-/*     */ import org.eclipse.cdt.core.dom.ast.gnu.cpp.GPPLanguage;
-/*     */ import org.eclipse.cdt.core.index.IIndex;
+ import java.io.PrintStream;
+ import java.util.HashMap;
+ import java.util.Map;
+ import org.eclipse.cdt.core.dom.ast.*;
+ import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTFieldReference;
+ import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTFunctionDeclarator;
+ import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTVisibilityLabel;
+ import org.eclipse.cdt.core.dom.ast.gnu.cpp.GPPLanguage;
+ import org.eclipse.cdt.core.index.IIndex;
 import org.eclipse.cdt.core.parser.DefaultLogService;
-/*     */ import org.eclipse.cdt.core.parser.FileContent;
-/*     */ import org.eclipse.cdt.core.parser.IParserLogService;
-/*     */ import org.eclipse.cdt.core.parser.IScannerInfo;
-/*     */ import org.eclipse.cdt.core.parser.IncludeFileContentProvider;
-/*     */ import org.eclipse.cdt.core.parser.ScannerInfo;
-/*     */ import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTFunctionDeclarator;
-/*     */import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTTranslationUnit;
-/*     */
-/*     */ public class ParserExample
-/*     */ {
-/*     */   public static void main(String[] args)
-/*     */     throws Exception
-/*     */   {
-/*  40 */     FileContent fileContent = FileContent.createForExternalFileLocation("/Users/schemer/cpptests/6.cpp");
-/*     */ 
-/*  45 */     Map definedSymbols = new HashMap();
-/*  46 */     String[] includePaths = new String[0];
-/*  47 */     IScannerInfo info = new ScannerInfo(definedSymbols, includePaths);
-/*  48 */     IParserLogService log = new DefaultLogService();
-/*     */
-/*  50 */     IncludeFileContentProvider emptyIncludes = IncludeFileContentProvider.getEmptyFilesProvider();
-/*     */
-/*  52 */     int opts = 8;
-/*  53 */     IASTTranslationUnit translationUnit = GPPLanguage.getDefault().getASTTranslationUnit(fileContent, info, emptyIncludes, null, opts, log);
-/*     */
-/*  56 */     IASTPreprocessorIncludeStatement[] includes = translationUnit.getIncludeDirectives();
-/*  57 */     for (IASTPreprocessorIncludeStatement include : includes) {
-/*  58 */       System.out.println("include - " + include.getName());
-/*     */     }
-/*     */ 
-/*  62 */     printTree(translationUnit, 1);
-/*     */ 
-/*  64 */     System.out.println("-----------------------------------------------------");
-/*  65 */     System.out.println("-----------------------------------------------------");
-/*  66 */     System.out.println("-----------------------------------------------------");
-/*     */ 
-///*  68 */     ASTVisitor visitor = new ASTVisitor()
-///*     */     {
-///*     */       public int visit(IASTName name)
-///*     */       {
-///*  73 */         if ((name.getParent() instanceof CPPASTFunctionDeclarator)) {
-///*  74 */           System.out.println("IASTName: " + name.getClass().getSimpleName() + "(" + name.getRawSignature() + ") - > parent: " + name.getParent().getClass().getSimpleName());
-///*  75 */           System.out.println("-- isVisible: " + ParserExample.isVisible(name));
-///*     */         }
-///*     */
-///*  78 */         return 3;
-///*     */       }
-//
-///*     */
-///*     */       public int visit(IASTDeclaration declaration)
-///*     */       {
-///*  83 */         System.out.println("declaration: " + declaration + " ->  " + declaration.getRawSignature());
-///*     */
-///*  85 */         if ((declaration instanceof IASTSimpleDeclaration)) {
-///*  86 */           IASTSimpleDeclaration ast = (IASTSimpleDeclaration)declaration;
-///*     */           try
-///*     */           {
-///*  89 */             System.out.println("--- type: " + ast.getSyntax() + " (childs: " + ast.getChildren().length + ")");
-///*  90 */             IASTNode typedef = ast.getChildren().length == 1 ? ast.getChildren()[0] : ast.getChildren()[1];
-///*  91 */             System.out.println("------- typedef: " + typedef);
-///*  92 */             IASTNode[] children = typedef.getChildren();
-///*  93 */             if ((children != null) && (children.length > 0))
-///*  94 */               System.out.println("------- typedef-name: " + children[0].getRawSignature());
-///*     */           }
-///*     */           catch (ExpansionOverlapsBoundaryException e)
-///*     */           {
-///*  98 */             e.printStackTrace();
-///*     */           }
-///*     */
-///* 101 */           IASTDeclarator[] declarators = ast.getDeclarators();
-///* 102 */           for (IASTDeclarator iastDeclarator : declarators) {
-///* 103 */             System.out.println("iastDeclarator > " + iastDeclarator.getName());
-///*     */           }
-///*     */
-///* 106 */           IASTAttribute[] attributes = ast.getAttributes();
-///* 107 */           for (IASTAttribute iastAttribute : attributes) {
-///* 108 */             System.out.println("iastAttribute > " + iastAttribute);
-///*     */           }
-///*     */
-///*     */         }
-///*     */
-///* 113 */         if ((declaration instanceof IASTFunctionDefinition)) {
-///* 114 */           IASTFunctionDefinition ast = (IASTFunctionDefinition)declaration;
-///* 115 */           IScope scope = ast.getScope();
-///*     */           try
-///*     */           {
-///* 118 */             System.out.println("### function() - Parent = " + scope.getParent().getScopeName());
-///* 119 */             System.out.println("### function() - Syntax = " + ast.getSyntax());
-///*     */           }
-///*     */           catch (DOMException e) {
-///* 122 */             e.printStackTrace();
-///*     */           } catch (ExpansionOverlapsBoundaryException e) {
-///* 124 */             e.printStackTrace();
-///*     */           }
-///* 126 */           ICPPASTFunctionDeclarator typedef = (ICPPASTFunctionDeclarator)ast.getDeclarator();
-///* 127 */           System.out.println("------- typedef: " + typedef.getName());
-///*     */         }
-///*     */
-///* 131 */         return 3;
-///*     */       }
-///*     */
-///*     */       public int visit(IASTTypeId typeId)
-///*     */       {
-///* 136 */         System.out.println("typeId: " + typeId.getRawSignature());
-///* 137 */         return 3;
-///*     */       }
-///*     */
-///*     */       public int visit(IASTStatement statement)
-///*     */       {
-///* 142 */         System.out.println("statement: " + statement.getRawSignature());
-///* 143 */         return 3;
-///*     */       }
-///*     */
-///*     */       public int visit(IASTAttribute attribute)
-///*     */       {
-///* 149 */         return 3;
-///*     */       }
-///*     */     };
-///* 154 */     visitor.shouldVisitNames = false;
-///* 155 */     visitor.shouldVisitDeclarations = false;
-///*     */ visitor.shouldVisitExpressions = true;
-///* 157 */     visitor.shouldVisitDeclarators = false;
-///* 158 */     visitor.shouldVisitAttributes = false;
-///* 159 */     visitor.shouldVisitStatements = false;
-///* 160 */     visitor.shouldVisitTypeIds = true;
-///*     */
-///* 162 */     translationUnit.accept(visitor);
-/*     */   }
-/*     */ 
-/*     */   private static void printTree(IASTNode node, int index) {
-/* 166 */     IASTNode[] children = node.getChildren();
-/*     */ 
-/* 168 */     boolean printContents = true;
-/*     */ 
-/* 170 */     if ((node instanceof CPPASTTranslationUnit)) {
-/* 171 */       printContents = false;
-/*     */     }
-/*     */
-/* 174 */     String offset = "";
-/*     */     try {
-/* 176 */       offset = node.getSyntax() != null ? " (offset: " + node.getFileLocation().getNodeOffset() + "," + node.getFileLocation().getNodeLength() + ")" : "";
-/* 177 */       printContents = node.getFileLocation().getNodeLength() < 30;
-/*     */     } catch (ExpansionOverlapsBoundaryException e) {
-/* 179 */       e.printStackTrace();
-/*     */     } catch (UnsupportedOperationException e) {
-/* 181 */       offset = "UnsupportedOperationException";
-/*     */     }
-/*     */ 
-/* 184 */     System.out.println(String.format(new StringBuilder("%1$").append(index * 2).append("s").toString(), new Object[] { "-" }) + node.getClass().getSimpleName() + offset + " -> " + (printContents ? node.getRawSignature().replaceAll("\n", " \\ ") : node.getRawSignature().subSequence(0, 5)));
-/*     */ 
-/* 186 */     for (IASTNode iastNode : children)
-/* 187 */       printTree(iastNode, index + 1);
-/*     */   }
-/*     */ 
-/*     */   public static boolean isVisible(IASTNode current)
-/*     */   {
-/* 194 */     IASTNode declator = current.getParent().getParent();
-/* 195 */     IASTNode[] children = declator.getChildren();
-/*     */ 
-/* 197 */     for (IASTNode iastNode : children) {
-/* 198 */       if ((iastNode instanceof ICPPASTVisibilityLabel)) {
-/* 199 */         return 1 == ((ICPPASTVisibilityLabel)iastNode).getVisibility();
-/*     */       }
-/*     */     }
-/*     */ 
-/* 203 */     return false;
-/*     */   }
-/*     */ }
+ import org.eclipse.cdt.core.parser.FileContent;
+ import org.eclipse.cdt.core.parser.IParserLogService;
+ import org.eclipse.cdt.core.parser.IScannerInfo;
+ import org.eclipse.cdt.core.parser.IncludeFileContentProvider;
+ import org.eclipse.cdt.core.parser.ScannerInfo;
+ import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTExpressionStatement;
+ import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTFunctionDeclarator;
+ import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTQualifiedName;
+ import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTTranslationUnit;
+
+ public class ParserExample
+ {
+   public static void main(String[] args)
+     throws Exception
+   {
+     FileContent fileContent = FileContent.createForExternalFileLocation("/Users/schemer/cpptests/infinity.cpp");
+ 
+     Map definedSymbols = new HashMap();
+     String[] includePaths = new String[0];
+     IScannerInfo info = new ScannerInfo(definedSymbols, includePaths);
+     IParserLogService log = new DefaultLogService();
+
+     IncludeFileContentProvider emptyIncludes = IncludeFileContentProvider.getEmptyFilesProvider();
+
+     int opts = 8;
+     IASTTranslationUnit translationUnit = GPPLanguage.getDefault().getASTTranslationUnit(fileContent, info, emptyIncludes, null, opts, log);
+
+     IASTPreprocessorIncludeStatement[] includes = translationUnit.getIncludeDirectives();
+     for (IASTPreprocessorIncludeStatement include : includes) {
+       System.out.println("include - " + include.getName());
+     }
+ 
+     printTree(translationUnit, 1);
+ 
+     System.out.println("-----------------------------------------------------");
+     System.out.println("-----------------------------------------------------");
+     System.out.println("-----------------------------------------------------");
+ 
+     ASTVisitor visitor = new ASTVisitor()
+     {
+       public int visit(IASTName name)
+       {
+//         if ((name.getParent() instanceof CPPASTFunctionDeclarator)) {
+//           System.out.println("IASTName: " + name.getClass().getSimpleName() + "(" + name.getRawSignature() + ") - > parent: " + name.getParent().getClass().getSimpleName());
+//           System.out.println("-- isVisible: " + ParserExample.isVisible(name));
+//         }
+         if(name instanceof CPPASTQualifiedName) {
+           System.out.println(name.toString());
+         }
+         if(name.isQualified()) {
+           System.out.println(name.toString());
+
+         }
+
+         return 3;
+       }
+
+       public int visit(IASTDeclaration declaration)
+       {
+         System.out.println("declaration: " + declaration + " ->  " + declaration.getRawSignature());
+
+         if ((declaration instanceof IASTSimpleDeclaration)) {
+           IASTSimpleDeclaration ast = (IASTSimpleDeclaration)declaration;
+           try
+           {
+             System.out.println("--- type: " + ast.getSyntax() + " (childs: " + ast.getChildren().length + ")");
+             IASTNode typedef = ast.getChildren().length == 1 ? ast.getChildren()[0] : ast.getChildren()[1];
+             System.out.println("------- typedef: " + typedef);
+             IASTNode[] children = typedef.getChildren();
+             if ((children != null) && (children.length > 0))
+               System.out.println("------- typedef-name: " + children[0].getRawSignature());
+           }
+           catch (ExpansionOverlapsBoundaryException e)
+           {
+             e.printStackTrace();
+           }
+
+           IASTDeclarator[] declarators = ast.getDeclarators();
+           for (IASTDeclarator iastDeclarator : declarators) {
+             System.out.println("iastDeclarator > " + iastDeclarator.getName());
+           }
+
+           IASTAttribute[] attributes = ast.getAttributes();
+           for (IASTAttribute iastAttribute : attributes) {
+             System.out.println("iastAttribute > " + iastAttribute);
+           }
+
+         }
+
+         if ((declaration instanceof IASTFunctionDefinition)) {
+           IASTFunctionDefinition ast = (IASTFunctionDefinition)declaration;
+           IScope scope = ast.getScope();
+           try
+           {
+             System.out.println("### function() - Parent = " + scope.getParent().getScopeName());
+             System.out.println("### function() - Syntax = " + ast.getSyntax());
+           }
+           catch (DOMException e) {
+             e.printStackTrace();
+           } catch (ExpansionOverlapsBoundaryException e) {
+             e.printStackTrace();
+           }
+           ICPPASTFunctionDeclarator typedef = (ICPPASTFunctionDeclarator)ast.getDeclarator();
+           System.out.println("------- typedef: " + typedef.getName());
+         }
+
+         return 3;
+       }
+
+       public int visit(IASTTypeId typeId)
+       {
+         System.out.println("typeId: " + typeId.getRawSignature());
+         return 3;
+       }
+
+       public int visit(IASTStatement statement)
+       {
+         System.out.println("statement: " + statement.getRawSignature());
+         return 3;
+       }
+
+       public int visit(IASTAttribute attribute)
+       {
+         return 3;
+       }
+     };
+     visitor.shouldVisitNames = true;
+     visitor.shouldVisitDeclarations = false;
+     visitor.shouldVisitExpressions = true;
+     visitor.shouldVisitDeclarators = false;
+     visitor.shouldVisitAttributes = false;
+     visitor.shouldVisitStatements = false;
+     visitor.shouldVisitTypeIds = true;
+
+     translationUnit.accept(visitor);
+   }
+ 
+   private static void printTree(IASTNode node, int index) {
+     IASTNode[] children = node.getChildren();
+ 
+     boolean printContents = true;
+ 
+     if ((node instanceof CPPASTTranslationUnit)) {
+       printContents = false;
+     }
+
+     String offset = "";
+     try {
+       offset = node.getSyntax() != null ? " (offset: " + node.getFileLocation().getNodeOffset() + "," + node.getFileLocation().getNodeLength() + ")" : "";
+       printContents = node.getFileLocation().getNodeLength() < 30;
+     } catch (ExpansionOverlapsBoundaryException e) {
+       e.printStackTrace();
+     } catch (UnsupportedOperationException e) {
+       offset = "UnsupportedOperationException";
+     }
+ 
+     System.out.println(String.format(new StringBuilder("%1$").append(index * 2).append("s").toString(), new Object[] { "-" }) + node.getClass().getSimpleName() + offset + " -> " + (printContents ? node.getRawSignature().replaceAll("\n", " \\ ") : node.getRawSignature().subSequence(0, 5)));
+ 
+     for (IASTNode iastNode : children)
+       printTree(iastNode, index + 1);
+   }
+ 
+   public static boolean isVisible(IASTNode current)
+   {
+     IASTNode declator = current.getParent().getParent();
+     IASTNode[] children = declator.getChildren();
+ 
+     for (IASTNode iastNode : children) {
+       if ((iastNode instanceof ICPPASTVisibilityLabel)) {
+         return 1 == ((ICPPASTVisibilityLabel)iastNode).getVisibility();
+       }
+     }
+ 
+     return false;
+   }
+ }
 
 /* Location:           /media/Dados/Codigos/C_Plus/Projetos/eclipse-cdt-standalone-astparser/bin/
  * Qualified Name:     ParserExample
